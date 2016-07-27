3280bb2c925239e96967d847aab98bd4fbbf5462
Alon <alon.home@gmail.com>
2016-07-27 12:17:26 -0700

    fixed hardcoded profile

--- StartScan.py
+++ StartScan.py
@@ -7,7 +7,7 @@ conn = Connection('http://127.0.0.1:5000/')
 print conn.get_version()
 
 # Define the target and configuration
-scan_profile = file('/home/alon/w3af/profiles/fast_scan.pw3af').read()
+scan_profile = file('./fast_scan.pw3af').read()
 target_urls = ['www.ynet.co.il']
 
 scan = Scan(conn)
