b88c1dba0bda62310a3c4f0195aaec25053acf1f
Alon <alon.home@gmail.com>
2016-07-27 11:22:21 -0700

    v2

--- GetFullScanResults.py
+++ GetFullScanResults.py
@@ -7,10 +7,6 @@ import inspect
 conn = Connection('http://127.0.0.1:5000/')
 print conn.get_version()
 
-# Define the target and configuration
-scan_profile = file('/home/alon/w3af/profiles/fast_scan.pw3af').read()
-target_urls = ['http://www.linnovate.net']
-
 #scan = Scan(conn)
 #scan.start(scan_profile, target_urls)
 scans = conn.get_scans()
