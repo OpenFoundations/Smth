8b3829ba1ead8f674ae4c87f6db0856fce989135
Alon <alon.home@gmail.com>
2016-07-28 02:21:20 -0700

    cleaned code

--- nodejs/GetScanStatus.js
+++ nodejs/GetScanStatus.js
@@ -1,4 +1,5 @@
 var Client = require('node-rest-client').Client;
+var fs = require("fs")
 
 var client = new Client();
 
@@ -6,20 +7,16 @@ var Target = 'http://www.ynet.co.il';
 var Profile = './fast_scan.pw3af';
 
 
-var fs = require("fs")
 function getProfile() {
 	data = fs.readFileSync(Profile, 'utf8')
 	return data;
 };
 
-//	var profile = getProfile();
-   var profile = getProfile();
-   var json = {'scan_profile': profile,  
-	"target_urls": [Target]};
-var args = {
-    'data': json ,
-    'headers': { "Content-Type": "application/json" }
-};
+var profile = getProfile();
+var json = {'scan_profile': profile,  
+	"target_urls": [Target]
+	};
+
 client.get("http://localhost:5000/scans/0/status", function (data, response) {
 	console.log(data.status);
 	//console.log(response);
